0c663d0ecfd67a87803db50264c8160dc2ba0601
Tomasz Węgrzyn <twgrzyn1@gmail.com>
2021-10-23 13:10:14 +0200

    Init spring boot backend

---
+++ src/main/java/com/example/slackpool/SlackPoolApplication.java
@@ -0,0 +1,13 @@
+package com.example.slackpool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackPoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlackPoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/slackpool/SlackPoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slackpool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlackPoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
