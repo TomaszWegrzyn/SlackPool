89b53e7f9e758b18e6cc86238322e3a05bb0a8a7
grzy512 <g.frak6+git@gmail.com>
2021-10-25 21:14:16 +0200

    Connect service to api

--- src/main/java/com/example/slackpool/client/SlackCommandsController.java
+++ src/main/java/com/example/slackpool/client/SlackCommandsController.java
@@ -1,5 +1,8 @@
 package com.example.slackpool.client;
 
+import com.example.slackpool.application.CreateQuestionnaireCommand;
+import com.example.slackpool.application.QuestionnaireService;
+import com.example.slackpool.domain.User;
 import com.slack.api.bolt.App;
 import com.slack.api.bolt.context.builtin.SlashCommandContext;
 import com.slack.api.bolt.request.builtin.SlashCommandRequest;
@@ -14,12 +17,19 @@ public class SlackCommandsController {
     @Autowired
     private App slackApp;
 
+    @Autowired
+    private QuestionnaireService questionnaireService;
+
     @PostConstruct
     public void init() {
-        slackApp.command("/question2", this::question2);
+        slackApp.command("/question2", this::createQuestionnaire);
     }
 
-    Response question2(SlashCommandRequest req, SlashCommandContext context) {
-        return context.ack(":wave: Hello World!");
+    Response createQuestionnaire(SlashCommandRequest req, SlashCommandContext context) {
+        var question = questionnaireService.create(CreateQuestionnaireCommand.builder()
+                .question(req.getPayload().getText())
+                .author(new User(req.getPayload().getUserId(), req.getPayload().getUserName()))
+                .build());
+        return context.ack("Created questionnaire " + question.getQuestion() + " " + question.getAnswerSummary());
     }
 }
