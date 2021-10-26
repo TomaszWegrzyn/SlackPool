b67c4041463aeef99d25ce04cc98c08d4673464a
grzy512 <g.frak6+git@gmail.com>
2021-10-26 18:10:57 +0200

    Add end state logic

--- src/main/java/com/example/slackpool/client/SlackCommandsController.java
+++ src/main/java/com/example/slackpool/client/SlackCommandsController.java
@@ -3,6 +3,7 @@ package com.example.slackpool.client;
 import com.example.slackpool.application.CreateQuestionnaireCommand;
 import com.example.slackpool.application.QuestionnaireService;
 import com.example.slackpool.domain.User;
+import com.example.slackpool.infrastructure.QuestionRepository;
 import com.slack.api.bolt.App;
 import com.slack.api.bolt.context.builtin.SlashCommandContext;
 import com.slack.api.bolt.request.builtin.SlashCommandRequest;
@@ -16,14 +17,16 @@ import javax.annotation.PostConstruct;
 public class SlackCommandsController {
     @Autowired
     private App slackApp;
-
     @Autowired
     private QuestionnaireService questionnaireService;
+    @Autowired
+    private QuestionRepository questionRepository;
 
     @PostConstruct
     public void init() {
         slackApp.command("/question", this::createQuestionnaire);
         slackApp.command("/endquestion", this::endQuestionnaire);
+        slackApp.command("/state", this::questionnaireState);
     }
 
     Response createQuestionnaire(SlashCommandRequest req, SlashCommandContext context) {
@@ -37,5 +40,14 @@ public class SlackCommandsController {
     private Response endQuestionnaire(SlashCommandRequest req, SlashCommandContext context) {
         var question = questionnaireService.end(req.getPayload().getText());
         return context.ack("Finished questionnaire " + question.getQuestion() + " " + question.getAnswerSummary());
+    }
+
+    private Response questionnaireState(SlashCommandRequest req, SlashCommandContext context) {
+        var question = questionRepository.findByQuestionAndEndTime(req.getPayload().getText(), null);
+
+        if (question.isPresent())
+            return context.ack(question.get().getAnswerSummary());
+        else
+            return context.ack("Not found opened question: " + req.getPayload().getText());
     }
 }
